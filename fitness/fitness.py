175af92ab1c5c043dc7bd2141f8809ab14f6004b
Erik Hemberg <erik.hemberg@gmail.com>
2018-09-22 18:22:10 +0100

    Edit remove prints

--- fitness/fitness.py
+++ fitness/fitness.py
@@ -45,7 +45,6 @@ class SRExpression(SRFitness):
         self.symbolic_expression = eval(fcn_str)
         fitness = self.run(cache)
         fitness = -fitness
-        print("{}: {}".format(fcn_str, fitness))
         return fitness
 
     def coev(self, fcn_str: str, tests: List[Individual], cache: Dict[str, float]) -> float:
@@ -54,7 +53,6 @@ class SRExpression(SRFitness):
             self.exemplars = eval(test.phenotype)
             fitness = self.__call__(fcn_str, cache)
             fitnesses[i] = fitness
-            print("{} vs {}: {}".format(fcn_str, test.phenotype, fitness))
 
         fitness = mean(fitnesses)
         return fitness
@@ -64,7 +62,6 @@ class SRExemplar(SRFitness):
     def __call__(self, fcn_str: str, cache: Dict[str, float]) -> float:
         self.exemplars = eval(fcn_str)
         fitness = self.run(cache)
-        print("{}: {}".format(fcn_str, fitness))
         return fitness
 
     def coev(self, fcn_str: str, tests: List[Individual], cache: Dict[str, float]) -> float:
@@ -73,7 +70,6 @@ class SRExemplar(SRFitness):
             self.symbolic_expression = eval(test.phenotype)
             fitness = self.__call__(fcn_str, cache)
             fitnesses[i] = fitness
-            print("{} vs {}: {}".format(fcn_str, test.phenotype, fitness))
 
         fitness = mean(fitnesses)
         return fitness
